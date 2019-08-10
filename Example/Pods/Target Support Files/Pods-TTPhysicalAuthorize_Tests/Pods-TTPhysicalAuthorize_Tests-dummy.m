bc90bc0be37e49bb55a037ea0b6ac4673227e373
Lincktang <Lincktang@users.noreply.github.com>
2019-08-10 17:14:32 +0800

    lincktang
    install TTPhysicalAuthorize

---
+++ Example/Pods/Target Support Files/Pods-TTPhysicalAuthorize_Tests/Pods-TTPhysicalAuthorize_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TTPhysicalAuthorize_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TTPhysicalAuthorize_Tests
+@end
---
+++ Example/Pods/Target Support Files/TTPhysicalAuthorize/TTPhysicalAuthorize-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TTPhysicalAuthorize : NSObject
+@end
+@implementation PodsDummy_TTPhysicalAuthorize
+@end
